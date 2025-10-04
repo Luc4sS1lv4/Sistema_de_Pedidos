7d590754e75867a25dc3d290c5472fae2990a524
Luc4sS1lv4 <lucas.lukas3245@gmail.com>
2025-10-03 23:19:32 -0300

    refactory: Order Service

--- src/application/service/OrderService.ts
+++ src/application/service/OrderService.ts
@@ -1,11 +1,11 @@
 
 import { Decimal } from "@prisma/client/runtime/library";
-import type { IOrder, ORDER} from "../../domain/Interfaces/IOrder.js";
+import type { IOrder, ORDER } from "../../domain/Interfaces/IOrder.js";
 import type { IProduct } from "../../domain/Interfaces/IProduct.js";
 
 export class OrderService {
     constructor(private OrderREpository: IOrder,
-                private ProductRepository: IProduct
+        private ProductRepository: IProduct
     ) { }
 
     async CreateOrder(ORDER: ORDER) {
@@ -14,15 +14,23 @@ export class OrderService {
 
         if (!total || !produto || !quantidade) throw new Error("por favor preencha todas as informações do produtos")
 
-        const preco_total: number[] = [0]
         const productExistis = await this.ProductRepository.findById(produto.podutoId)
 
-        if(!productExistis) throw new Error("produto bão existe")
-        
-           
+        if (!productExistis || productExistis.length === 0) {
+            throw new Error("produto não existe")
+        }
 
+        const preco_total = productExistis.reduce((acc, item) => {
+            return acc + (item.preco.toNumber() * quantidade)
+        }, 0)
 
-        
+        const totais = Decimal(preco_total)
+        return this.OrderREpository.save({
+            total: totais,
+            quantidade,
+            data_pedido: new Date(),
+            produto
+        })
 
     }
 
